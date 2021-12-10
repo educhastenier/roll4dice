628ecce7b0d8c96cc5bc22f166a928ec450c2693
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2021-12-10 17:54:19 +0100

    simplify kotlin code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,32 +10,18 @@ fun main() {
     println(possiblePairs)
 }
 
-fun makePossiblePairs(diceDraws: Array<DiceDraw>): MutableSet<PossiblePair> {
-    val pairs = hashSetOf<PossiblePair>()
-    val totalSum = diceDraws.sumBy { i -> i.value }
+fun makePossiblePairs(diceDraws: Array<DiceDraw>): MutableSet<Pair<Int, Int>> {
+    val pairs = hashSetOf<Pair<Int, Int>>()
+    val totalSum = diceDraws.sumBy { dice -> dice.value }
     for (i in 0..2) {
         val first = diceDraws[0].value + diceDraws[i + 1].value // associate the first one with each of other 3
-        pairs.add(PossiblePair(first, totalSum - first)) // so the other possible pair is the sum of the other 2
+        pairs.add(Pair(first, totalSum - first)) // so the other possible pair is the sum of the other 2
     }
     return pairs
 }
 
-fun rollDice(): DiceDraw = DiceDraw(getRandom())
+fun rollDice() = DiceDraw()
 
 fun getRandom(): Int = Random.Default.nextInt(1, 7)
 
-data class DiceDraw(val value: Int)
-data class PossiblePair(val n1: Int, val n2: Int) {
-
-    override fun toString(): String {
-        return "$n1 AND $n2"
-    }
-
-    override fun equals(other: Any?): Boolean {
-        return if (other is PossiblePair)
-            n1 == other.n1 && n2 == other.n2 || n1 == other.n2 && n2 == other.n1
-        else super.equals(other)
-    }
-
-    override fun hashCode() = n1 + n2
-}
+data class DiceDraw(val value: Int = getRandom())
